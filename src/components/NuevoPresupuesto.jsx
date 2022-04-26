4039915d6e794643ee3d63eb183c8543c2b92706
davatem17 <perezd625@gmail.com>
2022-04-25 21:03:08 -0500

    Components

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+        if(!Number(presupuesto)){
+            console.log('No es un numero')
+        } else {
+            console.log('Si es un numero')
+        }
+    }
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label>Definir Presupuesto</label>
                 <input
                     className='nuevo-presupuesto'
                     type="text"
                     placeholder='Añade tu Presupuesto'
+                    value={presupuesto}
+                    onChange={e => setPresupuesto(e.target.value)}
                 />
             </div>
             <input
