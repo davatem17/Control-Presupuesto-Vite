dc1cc8928e2f224dc5cb4cfcb17ffdcbb2e7b279
davatem17 <perezd625@gmail.com>
2022-04-28 15:53:55 -0500

    Control gasto

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import { useState } from "react"
+import Mensaje from "./Mensaje"
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState('')
     const handlePresupuesto = (e) => {
         e.preventDefault()
-        if(!Number(presupuesto)){
-            console.log('No es un numero')
-        } else {
-            console.log('Si es un numero')
-        }
+        if(!presupuesto || presupuesto < 0){
+            setMensaje('No es un presupuesto valido')
+            return
+        } 
+        setMensaje('')
+        setIsValidPresupuesto(true)
     }
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
@@ -16,16 +20,17 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
                 <label>Definir Presupuesto</label>
                 <input
                     className='nuevo-presupuesto'
-                    type="text"
+                    type="number"
                     placeholder='Añade tu Presupuesto'
                     value={presupuesto}
-                    onChange={e => setPresupuesto(e.target.value)}
+                    onChange={e => setPresupuesto(Number(e.target.value))}
                 />
             </div>
             <input
                 type="submit"
                 value="Añadir"
             />
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
     </div>
   )
